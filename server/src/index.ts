2db427dc3c580c04228bbe94873b5343501dd4d6
Rajneesh Mishra <rajneesh.mishra9616@gmail.com>
2024-10-11 11:05:44 +0530

    Initial Commit

---
+++ server/src/index.ts
@@ -0,0 +1,17 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get("/api/test", async (req: Request, res: Response) => {
+  res.json({ message: "Hey from test endpoint" }).status(200);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
